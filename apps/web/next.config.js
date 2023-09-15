aa02b914a893a99b114b878a76980ea911b1a08b
fleekgonzalo <114927947+fleekgonzalo@users.noreply.github.com>
2023-09-15 17:17:09 -0300

    Update next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -6,12 +6,12 @@ const headers = [{ key: 'Cache-Control', value: 'public, max-age=3600' }];
 const nextConfig = {
   productionBrowserSourceMaps: true,
   transpilePackages: ['data'],
-  output: 'export',
   reactStrictMode: false,
   experimental: {
     scrollRestoration: true,
     newNextLinkBehavior: true,
     swcPlugins: [['@lingui/swc-plugin', {}]]
-  },
+  }
+};
 
 module.exports = nextConfig;
